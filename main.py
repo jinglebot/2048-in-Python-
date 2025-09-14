193c5c47dd482f903f5ba8e34e0f20d72e711073
MARIA CRISTINA SALES AN <jingle1216@yahoo.com>
2025-09-13 21:20:01 -0700

    Update tile movement

--- main.py
+++ main.py
@@ -55,11 +55,17 @@ class Tile:
         text = FONT.render(str(self.value), 1, FONT_COLOR)
         window.blit(text, (self.x + RECT_WIDTH/2 - text.get_width()/2, self.y + RECT_HEIGHT/2 - text.get_height()/2))
         
-    def set_pos(self):
-        pass
+    def set_pos(self, ceil= False):
+        if ceil:
+            self.row = math.ceil(self.y / RECT_HEIGHT)
+            self.col = math.ceil(self.x / RECT_WIDTH)
+        else:
+            self.row = math.floor(self.y / RECT_HEIGHT)
+            self.col = math.floor(self.x / RECT_WIDTH)
         
     def move(self, delta):
-        pass
+        self.x += delta[0]
+        self.y += delta[1]
     
 def draw_grid(window):
     for row in range(1, ROWS):
@@ -93,7 +99,58 @@ def get_random_pos(tiles):
             break
         
     return row, col
-             
+
+def move_tiles(window, tiles, clock, direction):
+    updated = True
+    blocks = set()
+
+    if direction == "left":
+        sort_func = lambda x: x.col
+        reverse = False
+        delta = (-MOVE_VEL, 0)
+        boundary_check = lambda tile: tile.col == 0
+        get_next_tile = lambda tile: tiles.get(f"{tile.row}{tile.col-1}")
+        merge_check = lambda tile, next_tile: tile.x > next_tile.x + MOVE_VEL
+        move_check = lambda tile, next_tile: tile.x > next_tile.x + RECT_WIDTH + MOVE_VEL
+        ceil = True
+
+    if direction == "right":
+        pass
+    if direction == "up":
+        pass
+    if direction == "down":
+        pass
+
+    while updated:
+        clock.tick(FPS)
+        updated = False
+        sorted_tiles = sorted(tiles.values(), key=sort_func,reverse=reverse)
+
+        for i, tile in enumerate(sorted_tiles):
+            if boundary_check(tile):
+                continue
+
+            next_tile = get_next_tile(tile)
+            if not next_tile:
+                tile.move(delta)
+            elif (tile.value == next_tile.value and tile not in blocks and next_tile not in blocks):
+                if merge_check(tile, next_tile):
+                    tile.move(delta)
+                else:
+                    next_tile.value *= 2
+                    sorted_tiles.pop(i)
+                    blocks.add(next_tile)
+            elif move_check(tile, next_tile):
+                tile.move(delta)
+            else:
+                continue
+            
+            updated = True
+
+
+
+
+
 def generate_tiles():
     tiles = {}
     for _ in range(2):
